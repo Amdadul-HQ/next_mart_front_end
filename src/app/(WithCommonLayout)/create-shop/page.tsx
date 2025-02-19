88a9b9f4df6eb4d6f5d3fab2be3c8f5461453f42
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-02-19 15:43:47 +0600

    add create shop from style

--- src/app/(WithCommonLayout)/create-shop/page.tsx
+++ src/app/(WithCommonLayout)/create-shop/page.tsx
@@ -2,7 +2,7 @@ import CreateShopForm from "@/components/modules/shop/create-shop/CreateShopeFor
 
 const CreateShopePage = () => {
     return( 
-    <div>
+    <div className="flex justify-center items-center">
         <CreateShopForm/>
     </div>
     );
