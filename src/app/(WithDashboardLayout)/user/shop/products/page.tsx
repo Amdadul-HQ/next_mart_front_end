dfbaa7776ff5d57751773d09e86a1ca1f3ed5787
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-02-23 19:14:16 +0600

    add: table page nation

--- src/app/(WithDashboardLayout)/user/shop/products/page.tsx
+++ src/app/(WithDashboardLayout)/user/shop/products/page.tsx
@@ -1,8 +1,9 @@
 import ManageProducts from "@/components/modules/shop/products";
 import { getAllProducts } from "@/services/Product";
 
-const ManageProductsPage = async () => {
-  const { data, meta } = await getAllProducts();
+const ManageProductsPage = async ({searchParams}:{searchParams:Promise<{page:string}>}) => {
+  const {page} = await searchParams
+  const { data, meta } = await getAllProducts(page);
   return (
     <div>
       <ManageProducts meta={meta} products={data} />
