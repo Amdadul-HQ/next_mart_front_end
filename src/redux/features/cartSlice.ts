9503bbdd02ee1d0389d02420d0e236fabf366c1b
Amdadul-HQ <rimonamdadul301@gmail.com>
2025-02-24 19:21:42 +0600

    Feature:added cart feature

--- src/redux/features/cartSlice.ts
+++ src/redux/features/cartSlice.ts
@@ -1,5 +1,6 @@
 import { IProduct } from "@/types/product";
 import { createSlice } from "@reduxjs/toolkit";
+import { RootState } from "../store";
 interface InitialState {
     product:IProduct[],
 }
@@ -11,7 +12,17 @@ const initialState :InitialState = {
 const cartSlice = createSlice({
     name:"cart",
     initialState,
-    reducers:{}
+    reducers:{
+        addProduct:(state,action) => {
+            state.product.push(action.payload)
+        }
+    }
 })
+
+export const orderedProductsSelector = (state:RootState) => {
+    return state.cart.product
+}
+
+export const {addProduct} = cartSlice.actions
 
 export default cartSlice.reducer
